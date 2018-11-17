6d3d8012990ae5ce90133fb9a3ffd77433170cd9
comeonjy <1126254578@qq.com>
2018-11-18 01:39:24 +0800

    ckeditor

---
+++ application/admin/controller/Member.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Index extends Controller{
+    public function index(){
+        return view();
+    }
+
+    public function welcome(){
+        return view();
+    }
+
+    public function __empty($name){
+        return view(request()->controller().'/'.request()->action());
+    }
+    public function __call($a,$b){
+        return $a;
+        return view(request()->controller().'/'.request()->action());
+    }
+}
