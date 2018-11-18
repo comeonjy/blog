2260633a72307198e899195c76da238e7c5cc504
comeonjy <1126254578@qq.com>
2018-11-19 00:27:59 +0800

    qqlogin ok

--- application/index/controller/Passport.php
+++ application/index/controller/Passport.php
@@ -1,59 +1,81 @@
 <?php
+
 namespace app\index\controller;
 
 use QQ\QC;
+use think\Controller;
+use think\Db;
+use think\Session;
 
-class Passport
+class Passport extends Controller
 {
     public function login()
     {
-        if (request()->isPost()){
-            sucReturn('ok');
-        }else{
+        if (request()->isPost()) {
+            var_dump(input(''));die;
+        } else {
             return view();
         }
     }
-    function qqLogin(){
-//        $qc = new QC();
-//        $arr = $qc->get_user_info();
 
-
+    function qqLogin()
+    {
         $qc = new QC();
-        $acs = $qc->qq_callback();
-        $oid = $qc->get_openid();
-        $qc = new QC($acs,$oid);
-        $arr = $qc->get_user_info();
-
-
-        echo '<meta charset="UTF-8">';
-        echo "<p>";
-        echo "Gender:".$arr["gender"];
-        echo "</p>";
-        echo "<p>";
-        echo "NickName:".$arr["nickname"];
-        echo "</p>";
-        echo "<p>";
-        echo "<img src=\"".$arr['figureurl']."\">";
-        echo "<p>";
-        echo "<p>";
-        echo "<img src=\"".$arr['figureurl_1']."\">";
-        echo "<p>";
-        echo "<p>";
-        echo "<img src=\"".$arr['figureurl_2']."\">";
-        echo "<p>";
-        echo "vip:".$arr["vip"];
-        echo "</p>";
-        echo "level:".$arr["level"];
-        echo "</p>";
-        echo "is_yellow_year_vip:".$arr["is_yellow_year_vip"];
-        echo "</p>";
+        $qc->qq_login();
     }
+
+    public function logout(){
+        Session::delete('userinfo');
+        $this->redirect('index/index');
+    }
+
     public function register()
     {
         return view();
     }
+
+    public function doLogin($user_openid)
+    {
+        $user = Db::name('user')->where('user_openid', $user_openid)->find();
+        Session::set('userinfo', $user);
+    }
+
     public function qqCallback()
     {
-        echo 'this is qqCallback';
+        $qc = new QC();
+        $qc->qq_callback();
+        $openid = $qc->get_openid();
+
+        $res = Db::name('user')->where('qq_openid', $openid)->find();
+        if (!empty($res)) {
+            $qqbind = Session::get('qqbind');
+            if (!$qqbind) {
+                //登录
+                $data['last_login'] = time();
+                Db::name('user')->where('qq_openid', $openid)->update($data);
+                $this->doLogin($res['user_openid']);
+                $this->redirect('index/index');
+            } else {
+                //绑定(待完善)
+                Db::name('user')->where('user_openid', $res['user_openid'])->update(array('qq_openid' => $openid));
+                $this->redirect('index/index');
+            }
+        } else {
+            $arr = $qc->get_user_info();
+            //注册
+            $data['nickname'] = $arr["nickname"];
+            $data['figureurl'] = $arr["figureurl_qq_2"];
+            $data['gender'] = $arr["gender"];
+            $data['province'] = $arr["province"];
+            $data['city'] = $arr["city"];
+            $data['year'] = $arr["year"];
+            $data['user_openid'] = md5(microtime() . rand(1000, 9999));
+            $data['qq_openid'] = $openid;
+            $data['create_time'] = time();
+            $data['last_login'] = time();
+            Db::name('user')->insert($data);
+            $this->doLogin($data['user_openid']);
+            $this->redirect('index/index');
+        }
     }
 }
